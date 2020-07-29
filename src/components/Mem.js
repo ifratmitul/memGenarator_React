59d21c87e7d00883acac2a5fc2cfdf45b779b9b4
ifratmitul <ifrat.mitul@hotmail.com>
2020-07-29 13:49:02 +0600

    css initialized & headed added

---
+++ src/components/Mem.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class Mem extends Component {
+    render() {
+
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Mem
