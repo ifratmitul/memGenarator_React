aad74e301c2f00248661d1f44b93367e4654c41d
ifratmitul <ifrat.mitul@hotmail.com>
2020-07-29 14:47:28 +0600

    form added

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 import './App.css';
 import Header from './components/Header'
+import Mem from './components/Mem'
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Mem/>
     </div>
   );
 }
